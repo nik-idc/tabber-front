80401ee81460a6f735303e72388e4f3626d09380
Nikita Vypritsky <nikvmain@gmail.com>
2023-05-18 10:41:12 +0300

    Forgot to commit some other files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './components/signin/signin.component';
+import { SignUpComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+import { UserComponent } from './components/user/user.component';
+import { TabComponent } from './components/tab/tab.component';
+
+const routes: Routes = [
+  {path: '', component: SignInComponent},
+  {path: 'signin', component: SignInComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'user', component: UserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const RoutingComponents = [
+  SignInComponent,
+  SignUpComponent,
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/signin/signin.component';
+import { SignUpComponent } from './components/signup/signup.component'
+import { MaterialModule } from './material/material.module';
+
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
+import { UserComponent } from './components/user/user.component';
+import { TabComponent } from './components/tab/tab.component';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    UserComponent,
+    TabComponent,
+    RoutingComponents,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
